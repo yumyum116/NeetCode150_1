ebf46041791caef865efdd4442f0ac86b9fbc9db
yumyum116 <yu24miura@gmail.com>
2025-11-04 23:10:48 +0900

    Implement containsDuplicate method in Solution class

---
+++ containsDuplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        i = 0
+        while i < len(nums) - 1:
+            j = i + 1
+            while j < len(nums):
+                if nums[i] == nums[j]:
+                    return True
+                j += 1
+            i += 1
+        return False
