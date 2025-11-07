1c3b135e0a1ae63b9b7ee9ff9d937c863aa91ed5
yumyum116 <yu24miura@gmail.com>
2025-11-07 23:49:20 +0900

    Revise comments for groupAnagrams function
    Updated comments to reflect approach, results, strengths, and limitations.

--- array/Group Anagrams/groupAnagrams.py
+++ array/Group Anagrams/groupAnagrams.py
@@ -1,7 +1,7 @@
-# Approach    :
-# Result      :
-# Strength    : 
-# Limitation  : 
+# Approach    : Sorting
+# Result      : runtime -> 0.712 sec, memory -> 52.8 MB
+# Strength    : simple and understandable
+# Limitation  : time consuming O(m * nlogn), where m is the number of strings and n is the length of the longest string.
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
