ae11fffdd866a6187f3708f5f76533111f68be3d
yumyum116 <yu24miura@gmail.com>
2025-11-07 23:44:30 +0900

    Add groupAnagrams function to group anagrams
    Implement a solution to group anagrams from a list of strings.

---
+++ array/Group Anagrams/groupAnagrams.py
@@ -0,0 +1,12 @@
+# Approach    :
+# Result      :
+# Strength    : 
+# Limitation  : 
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for s in strs:
+            key = "".join(sorted(s))
+            groups[key].append(s)
+        return list(groups.values())
