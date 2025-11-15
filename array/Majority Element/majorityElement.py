5814cc72cb2900a8cdc4d32857ee12829185ba9a
yumyum116 <yu24miura@gmail.com>
2025-11-15 21:13:21 +0900

    Create majorityElement.py

---
+++ array/Majority Element/majorityElement.py
@@ -0,0 +1,13 @@
+# Approach    : Built-in Approach
+# Results     : runtime -> 5 ms, memory -> 19.38 MB
+# Strength    : relatively faster and efficient more han other approaches
+# Limitation  : 
+
+import collections
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        map = collections.Counter(nums)
+        map = map.most_common()
+        if map[0][1] > len(nums) / 2:
+            return map[0][0]
