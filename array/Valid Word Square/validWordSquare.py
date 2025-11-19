bef31fb02606e6750d7477de9d79613881dbbcc3
yumyum116 <yu24miura@gmail.com>
2025-11-19 23:40:12 +0900

    Refactor validWordSquare with hash map approach
    Updated the algorithm to use a hash map for improved performance and added detailed comments regarding approach, results, strengths, and limitations.

--- array/Valid Word Square/validWordSquare.py
+++ array/Valid Word Square/validWordSquare.py
@@ -1,19 +1,22 @@
-# Approach    :
-# Results     :
-# Strength    :
-# Limitation  :
+# Approach    : Hash Map
+# Results     : runtime -> 0.865 sec, memory -> 52 MB
+# Strength    : easy to debug and high scalability
+# Limitation  : additional memory required O(N^2)
 
 class Solution:
     def validWordSquare(self, words: List[str]) -> bool:
         column_words = [[0]]
         for i in range(len(words)):
-            for j in range(len(word[i])):
-                if j == column_words[i][0]:
-                    column_words[i].append(words[i][j])
+            for j in range(len(words[i])):
+                if j < len(column_words) and j == column_words[j][0]:
+                    column_words[j].append(words[i][j])
                 else:
-                    column_words[i + 1].append([j, words[i][j]])
+                    column_words.append([j, words[i][j]])
         for i in range(len(words)):
-            if words[i] != column_words[i][1]:
+            if len(words[i]) != len(column_words[i]) - 1:
                 return False
+            for j in range(len(words[i])):
+                if words[i][j] != column_words[i][j + 1]:
+                    return False
         else:
             return True
